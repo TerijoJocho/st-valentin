907d9e7b50b959bf1840bbd3f00587fd9687b9fb
TerijoJocho <130741698+TerijoJocho@users.noreply.github.com>
2026-02-02 02:35:14 +0100

    add header, main and footer

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import "./Main.css";
+import Heart from "./Heart.jsx";
+
+export default function Main() {
+  return (
+    <main>
+      <p>Clique sur un coeur !</p>
+      <Heart />
+    </main>
+  );
+}
