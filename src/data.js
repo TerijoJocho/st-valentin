ac617ca19030d2acb1da13637d8547720e25d6e3
TerijoJocho <130741698+TerijoJocho@users.noreply.github.com>
2026-02-03 21:50:21 +0100

    test deploy

---
+++ src/data.js
@@ -0,0 +1,92 @@
+export const data = [
+    {
+        id: 1,
+        icon: "heart",
+        text: "Tu es le chant de la tourterelle dans ma vie",
+        date: "2026-02-02"
+    },
+    {
+        id: 2,
+        icon: "heart",
+        text: "Jolie, Intelligente, Drole sont les meilleurs mots qui te definissent (et bien d'autre)",
+        date: "2026-02-15"
+    },
+    {
+        id: 3,
+        icon: "heart",
+        text: "Je souhaite que tu sois mon bonheur et ma faveur pour toujours (Proverbes 18:22)",
+        date: "2026-02-16"
+    },
+    {
+        id: 4,
+        icon: "heart",
+        text: "Hâte de cuisiner des sushis avec ma personne préférée (c'est toi)",
+        date: "2026-02-17"
+    },
+    {
+        id: 5,
+        icon: "heart",
+        text: "Envie de danser avec toi sur notre musique comme au nouvel an",
+        date: "2026-02-18"
+    },
+    {
+        id: 6,
+        icon: "heart",
+        text: "Tu es forte mon amour, et même lorsque tu te sens au plus bas et faible rappel toi ce que dit 2 Corinthiens 12:9-10",
+        date: "2026-02-19"
+    },
+    {
+        id: 7,
+        icon: "heart",
+        text: "Rappel toi de respirer ! Arrête de stresser mon amour, remet tout entre les mains de Dieu car tu es la prunelle de ses yeux",
+        date: "2026-02-20"
+    },
+    {
+        id: 8,
+        icon: "heart",
+        text: "Je t'aime de tout mon coeur",
+        date: "2026-02-21"
+    },
+    {
+        id: 9,
+        icon: "heart",
+        text: "Pressé qu'on habite ensemble, on pourra regarder Nana à deux sous le plaide",
+        date: "2026-02-22"
+    },
+    {
+        id: 10,
+        icon: "heart",
+        text: "Je prie que tu cherches Dieu de tout ton coeur car il n'y a que lui qui peut te combler entièrement et parce qu'il t'aime d'un amour inconditionnel",
+        date: "2026-02-23"
+    },
+    {
+        id: 11,
+        icon: "heart",
+        text: "Tout va bien mon coeur, ne laisse pas tes émotions controler toutes tes pensées. Fais attention à ce que tu dis sur ta vie, Proverbes 21:23",
+        date: "2026-02-24"
+    },
+    {
+        id: 12,
+        icon: "heart",
+        text: "Ça te dit un phô si on a l'argent ?",
+        date: "2026-02-25"
+    },
+    {
+        id: 13,
+        icon: "heart",
+        text: "Envie de voyager. dans pleins de pays avec toi et manger toute sorte de spécialité, imagine genre le tour de l'Afrique popopo",
+        date: "2026-02-26"
+    },
+    {
+        id: 14,
+        icon: "heart",
+        text: "J'aime trop tes yeux, ton regard, j'aime tes joues et tes lèvres, j'aime tes douce mains, j'aime tes beaux cheveux, j'aime ta voix et ton rire me fait craquer",
+        date: "2026-02-27"
+    },
+    {
+        id: 15,
+        icon: "heart",
+        text: "I NEED YOUR LOOOOOOVE 🤟🏾, j'espère que t'as kiffé ce calendrier de l'après mdr, je t'aime fort fort mon amour !",
+        date: "2026-02-28"
+    }
+];
