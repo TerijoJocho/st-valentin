0f16f1c6cf4a607feffc79b4fe4dcce8ce473e32
TerijoJocho <130741698+TerijoJocho@users.noreply.github.com>
2026-02-03 11:14:49 +0100

    add responsive pour tel et tablette

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,9 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faCopyright } from "@fortawesome/free-solid-svg-icons"
 import "./Footer.css"
 
 export default function Footer() {
     return (
         <footer>
-            <p className="footer-title">Daryl</p>
-            <FontAwesomeIcon icon={faCopyright} />
+            <p className="footer-title">Daryl ©</p>
         </footer>
     )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,12 @@ import { faCalendarDays } from "@fortawesome/free-solid-svg-icons";
 export default function Header() {
     
     function getToday() {
-        return new Date().toISOString().split("T")[0];
+        // return new Date().toISOString().split("T")[0];
+        const now = new Date();
+        const y = now.getFullYear();
+        const m = String(now.getMonth() + 1).padStart(2, "0");
+        const d = String(now.getDate()).padStart(2, "0");
+        return `${d}-${m}-${y}`;
     }
     
     const currDate = getToday();
