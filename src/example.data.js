e5a165674a71124fccf2440f7493f9aa55e43549
TerijoJocho <130741698+TerijoJocho@users.noreply.github.com>
2026-02-03 11:34:34 +0100

    Remove data.js from repo and add example template

---
+++ src/example.data.js
@@ -0,0 +1,98 @@
+export const data = [
+    {
+        id: 0,
+        icon: "heart",
+        text: "Message d'exemple",
+        date: "2026-02-02"
+    },
+    {
+        id: 5,
+        icon: "heart",
+        text: "Message d'exemple",
+        date: "2026-02-14"
+    },
+    {
+        id: 14,
+        icon: "heart",
+        text: "Message d'exemple",
+        date: "2026-02-15"
+    },
+    {
+        id: 7,
+        icon: "heart",
+        text: "Message d'exemple",
+        date: "2026-02-16"
+    },
+    {
+        id: 4,
+        icon: "heart",
+        text: "Message d'exemple caca",
+        date: "2026-02-17"
+    },
+    {
+        id: 1,
+        icon: "heart",
+        text: "Message d'exemple",
+        date: "2026-02-18"
+    },
+    {
+        id: 6,
+        icon: "heart",
+        text: "Message d'exemple",
+        date: "2026-02-19"
+    },
+    {
+        id: 3,
+        icon: "heart",
+        text: "Message d'exemple",
+        date: "2026-02-20"
+    },
+    {
+        id: 15,
+        icon: "heart",
+        text: "Message d'exemple",
+        date: "2026-02-21"
+    },
+    {
+        id: 12,
+        icon: "heart",
+        text: "Message d'exemple",
+        date: "2026-02-22"
+    },
+    {
+        id: 8,
+        icon: "heart",
+        text: "Message d'exemple",
+        date: "2026-02-23"
+    },
+    {
+        id: 11,
+        icon: "heart",
+        text: "Message d'exemple",
+        date: "2026-02-24"
+    },
+    {
+        id: 9,
+        icon: "heart",
+        text: "Message d'exemple",
+        date: "2026-02-25"
+    },
+    {
+        id: 2,
+        icon: "heart",
+        text: "Message d'exemple",
+        date: "2026-02-26"
+    },
+    {
+        id: 10,
+        icon: "heart",
+        text: "Message d'exemple",
+        date: "2026-02-27"
+    },
+    {
+        id: 13,
+        icon: "heart",
+        text: "Message d'exemple",
+        date: "2026-02-28"
+    }
+];
